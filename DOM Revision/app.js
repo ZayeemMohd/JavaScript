f1b59c265e110ed7c5eb65132b163a72bb651a78
ZayeemMohd <zayeemudin123@gmail.com>
2023-12-07 19:32:42 +0530

    misc

--- DOM Revision/app.js
+++ DOM Revision/app.js
@@ -58,60 +58,68 @@
 
 //selecting Elements
 
-    let body = document.querySelector('body');
+//     let body = document.querySelector('body');
     
 
-// creating elements
+// // creating elements
 
-    let redP = document.createElement('p');
-    let blueH3 = document.createElement('h3');
-    let adiv = document.createElement('div');
-    let inH1 = document.createElement('h1');
-    let meTooP = document.createElement('p');
-    let inp = document.createElement('input');
-    let subBtn = document.createElement("button");
-    let heading = document.createElement('h1')
-    let para = document.createElement('p')
+//     let redP = document.createElement('p');
+//     let blueH3 = document.createElement('h3');
+//     let adiv = document.createElement('div');
+//     let inH1 = document.createElement('h1');
+//     let meTooP = document.createElement('p');
+//     let inp = document.createElement('input');
+//     let subBtn = document.createElement("button");
+//     let heading = document.createElement('h1')
+//     let para = document.createElement('p')
 
 
-// adding elements
+// // adding elements
 
-    body.append(redP);
-    body.append(blueH3);
-    body.insertAdjacentElement('beforeend',adiv);
-    adiv.prepend(inH1)
-    adiv.append(meTooP)
-    body.append(inp);
-    inp.insertAdjacentElement('afterend',subBtn);
-    body.insertAdjacentElement('afterbegin',heading)
-    heading.insertAdjacentElement('afterend',para);
+//     body.append(redP);
+//     body.append(blueH3);
+//     body.insertAdjacentElement('beforeend',adiv);
+//     adiv.prepend(inH1)
+//     adiv.append(meTooP)
+//     body.append(inp);
+//     inp.insertAdjacentElement('afterend',subBtn);
+//     body.insertAdjacentElement('afterbegin',heading)
+//     heading.insertAdjacentElement('afterend',para);
 
-// performing operations
+// // performing operations
 
-    redP.innerText = "Hey I'm red!"
-    redP.style.color = 'red'
+//     redP.innerText = "Hey I'm red!"
+//     redP.style.color = 'red'
 
-    blueH3.innerText = "I'm a blue h3"
-    blueH3.style.color = 'blue'
+//     blueH3.innerText = "I'm a blue h3"
+//     blueH3.style.color = 'blue'
 
-    adiv.style.border = "1px solid black"
-    adiv.style.backgroundColor = 'pink'
-    adiv.style.margin = '20px auto'
+//     adiv.style.border = "1px solid black"
+//     adiv.style.backgroundColor = 'pink'
+//     adiv.style.margin = '20px auto'
 
-    inH1.innerText = "I'm in a div"
-    meTooP.innerText = "ME TOO!"
+//     inH1.innerText = "I'm in a div"
+//     meTooP.innerText = "ME TOO!"
 
-    inp.setAttribute('placeholder','username')
+//     inp.setAttribute('placeholder','username')
 
-    subBtn.append('Click me');
-    subBtn.setAttribute('id','btn');
+//     subBtn.append('Click me');
+//     subBtn.setAttribute('id','btn');
 
-    let woIcBTN = document.querySelector('#btn');
-    woIcBTN.style.backgroundColor = 'blue';
-    woIcBTN.style.color = 'white';
+//     let woIcBTN = document.querySelector('#btn');
+//     woIcBTN.style.backgroundColor = 'blue';
+//     woIcBTN.style.color = 'white';
 
-    heading.innerHTML = 'DOM Practice';
-    heading.style.textDecoration = 'underline';
-    heading.style.color = 'purple'
+//     heading.innerHTML = 'DOM Practice';
+//     heading.style.textDecoration = 'underline';
+//     heading.style.color = 'purple'
 
-    para.innerHTML = 'Hello world <b>Practice</b> karo!'
+//     para.innerHTML = 'Hello world <b>Practice</b> karo!'
+
+let img = document.querySelector(".oldImg");
+
+    let leftArrow = document.querySelector('.right-arrow');
+
+    leftArrow.addEventListener('click',()=>{
+        img.setAttribute('src',"")
+    })
